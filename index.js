4c3e2b24fcb886db08eabd59ba46834270b4dd08
Grace Nduta <ndutak5674@gmail.com>
2025-03-07 08:43:46 +0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+var setBestCustomer;
+function setBestCustomer(){
+    bestCustomer="not bob";
+}
+const LeastFavoriteCustomer="someone";
+
+function overwriteBestCustomer(newBestCustomer){
+    bestCustomer=newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer="new person"
+}
+
+
+
